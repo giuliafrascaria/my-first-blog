5c2eefdf8120d30d2ba5ee8cec133d0bae9d969a
Giulia <giulia.frascaria@gmail.com>
2015-12-12 15:07:37 +0100

    Ho cambiato l'HTML per il mio sito

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+
+
+"""
+abbiamo creato un metodo (def) chiamato post_list 
+che prende request e restituisce un metodo render 
+che ci fornirà (metterà insieme) il nostro template 
+blog/post_list.html
+"""
